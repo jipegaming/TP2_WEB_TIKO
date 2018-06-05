b9a0482f483ebd29ddc1b539f9d9bfc498925ac4
Mana987 <mana.longine@gmail.com>
2018-06-05 08:30:47 -1000

    add pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Carriere from './components/layouts/Carriere';
 import Reseaux from './components/layouts/Reseaux';
 import Projets from './components/layouts/Projets';
 import Footer from './components/layouts/Footer';
+import Contact from './components/layouts/Contact';
+import Porfolio from './components/layouts/Portfolio';
 
 class App extends Component {
   render() {
---
+++ client/src/components/layouts/Portfolio.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Porfolio extends Component {
+    render() {
+        return ( <div>Bibliographie </div>
+        )
+    }
+}
+
+export default Porfolio;
