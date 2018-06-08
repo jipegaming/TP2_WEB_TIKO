df4bd661cee8d78204adf186909f6e08a1573fdb
mana987 <mlongine.tcc@gmail.com>
2018-06-07 20:28:26 -1000

    maj

--- client/src/components/layouts/Carriere.js
+++ client/src/components/layouts/Carriere.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Media,FormGroup } from 'react-bootstrap/lib/';
+import { Grid, Row, Col, Media, FormGroup } from 'react-bootstrap/lib/';
 
 function FieldGroup({ id, image, subtitle, text }) {
     return (
-        <FormGroup controlId = {id}>
-            <Row className   = "show-grid">
+        <FormGroup controlId={id}>
+            <Row className="show-grid">
                 <Col>
                     <Media.Left>
-                        <img width={64} height={64} alt="thumbnail" src={image}/>
+                        <img width={64} height={64} alt="thumbnail" src={image} />
                     </Media.Left>
 
                     <Media.Body>
@@ -27,56 +27,56 @@ class Carriere extends Component {
 
     render() {
 
-        const articleInstance =(
+        const articleInstance = (
 
             <Grid id="carriere">
 
-        <Row style={{ paddingTop: 50, paddingBottom: 30 }}>
-            <Col >
-                <h2>CARRIÈRE PROFESSIONNELLE</h2>
-            </Col>
-        </ Row>
+                <Row style={{ paddingTop: 50, paddingBottom: 30 }}>
+                    <Col >
+                        <h2>CARRIÈRE PROFESSIONNELLE</h2>
+                    </Col>
+                </ Row>
 
+                <FieldGroup
+                    id="carrierePro"
+                    image=""
+                    subtitle="Biographie"
+                    text="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                    ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at,
+                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                fringilla. Donec lacinia congue felis in faucibus."
+                />
+        
         <FieldGroup
-        id="carrierePro"
-        image=""
-        subtitle="Biographie"
-        text="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-        fringilla. Donec lacinia congue felis in faucibus."
-        />
-
+                    id="carrierePro"
+                    image=""
+                    subtitle="Style Architectural"
+                    text="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                    ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at,
+                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                fringilla. Donec lacinia congue felis in faucibus."
+                />
+        
         <FieldGroup
-        id="carrierePro"
-        image=""
-        subtitle="Style Architectural"
-        text="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-        fringilla. Donec lacinia congue felis in faucibus."
-        />
-
+                    id="carrierePro"
+                    image=""
+                    subtitle="Projets de références"
+                    text="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                    ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at,
+                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                fringilla. Donec lacinia congue felis in faucibus."
+                />
+        
         <FieldGroup
-        id="carrierePro"
-        image=""
-        subtitle="Projets de références"
-        text="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-        fringilla. Donec lacinia congue felis in faucibus."
-        />
-
-        <FieldGroup
-        id="carrierePro"
-        img=""
-        subtitle="Méthode de travail"
-        text="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-        fringilla. Donec lacinia congue felis in faucibus."
-        />
-
+                    id="carrierePro"
+                    img=""
+                    subtitle="Méthode de travail"
+                    text="Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                    ante sollicitudin commodo.Cras purus odio, vestibulum in vulputate at,
+                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                fringilla. Donec lacinia congue felis in faucibus."
+                />
+        
         </Grid>
         )
 
@@ -89,5 +89,5 @@ class Carriere extends Component {
 
 export default Carriere;
 
-       
+
 
