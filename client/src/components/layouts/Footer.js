b3232de7f71b57330b4270f9de3176f691d0bfb7
Manaarii LONGINE <manaariilongine@MBP-Manaarii-LONGINE.local>
2018-06-22 15:39:09 -1000

    up

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import {Jumbotron, Button } from 'react-bootstrap/lib/';
+import {div, Button } from 'react-bootstrap/lib/';
 
 class Footer extends Component {
     render() {
         return (
-            <Jumbotron style={{ paddingTop : 50}} className="text-center">
-                <h1>Hello, world!</h1>
+            <div style={{ paddingTop : 50}} className="text-center">
                 <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
-                </p>
+                "L'architecture, c'est une tournure d'esprit et non un métier" 
+                </p> 
                 <p>
-                    <Button bsStyle="primary">Learn more</Button>
-                </p>
-            </Jumbotron>
+                Copyright©
+                </p>              
+            </div>
         )
     }
 }
--- client/src/components/layouts/Reseaux.js
+++ client/src/components/layouts/Reseaux.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Media, FormGroup } from 'react-bootstrap/lib/';
+import { Grid, Row, Col} from 'react-bootstrap/lib/';
 import { Parallax } from 'react-parallax';
 
 const image1 = "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D";
