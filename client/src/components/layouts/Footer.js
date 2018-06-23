169df6dc9983e55503ad0442c40d6f31f4b6cdab
Manaarii LONGINE <manaariilongine@MBP-Manaarii-LONGINE.local>
2018-06-23 09:24:44 -1000

    citation footer

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
         return (
             <div style={{ paddingTop : 50}} className="text-center">
                 <p>
-                "L'architecture, c'est une tournure d'esprit et non un métier" 
+                "L'architecture, c'est une tournure d'esprit et non un métier" - Le Corbusier (1887 - 1965)
                 </p> 
                 <p>
                 Copyright©
